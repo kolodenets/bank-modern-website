db5206e44b3214c1c1b02ba7cfdfd0ff03ba491e
Mikhail Kolodenets <mishakolode@gmail.com>
2023-02-28 22:14:02 +0300

    feat: add components, fill navbar, start hero section

---
+++ modern-bank-website/src/components/GetStarted.jsx
@@ -0,0 +1,11 @@
+import styles from '../style'
+import { arrowUp} from '../assets'
+
+const GetStarted = () => (
+    <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient`}>
+      
+    </div>
+  )
+
+
+export default GetStarted
