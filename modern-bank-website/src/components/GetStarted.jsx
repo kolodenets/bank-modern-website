c506e53348a8134b3d995d895c006abf1f85e82e
Mikhail Kolodenets <mishakolode@gmail.com>
2023-03-02 19:26:17 +0300

    feat: add Hero, Stats sections ang GetStarted button

--- modern-bank-website/src/components/GetStarted.jsx
+++ modern-bank-website/src/components/GetStarted.jsx
@@ -1,11 +1,28 @@
-import styles from '../style'
-import { arrowUp} from '../assets'
+import styles from "../style";
+import { arrowUp } from "../assets";
 
 const GetStarted = () => (
-    <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient`}>
-      
+  <div
+    className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer  `}
+  >
+    <div
+      className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full hover:shadow-[0px_0px_75px_-10px_rgb(9,223,252,1),inset_0px_0px_35px_-10px_rgb(9,223,252,1)]`}
+    >
+      <div className={`${styles.flexStart} flex-row`}>
+        <p className="font-poppins font-medium text-[18px] leading-[23px] mr-2">
+          <span className="text-gradient">Get</span>
+        </p>
+        <img
+          src={arrowUp}
+          alt="arrow"
+          className="w-[23px] h-[23px] object-contain"
+        />
+      </div>
+      <p className="font-poppins font-medium text-[18px] leading-[23px]">
+        <span className="text-gradient">Started</span>
+      </p>
     </div>
-  )
+  </div>
+);
 
-
-export default GetStarted
+export default GetStarted;
